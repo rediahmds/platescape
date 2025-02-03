5e4a449fc0af868f7222918bc04c7a00ecc80b54
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-04 00:49:00 +0700

    feat(network): :sparkles: generate image url
    concatenating base url with images endpoint

--- lib/data/api/api_services.dart
+++ lib/data/api/api_services.dart
@@ -2,12 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:platescape/data/models/models.dart';
 
 class APIServices {
-  static final _options =
-      BaseOptions(baseUrl: "https://restaurant-api.dicoding.dev/");
+  static const String _baseUrl = "https://restaurant-api.dicoding.dev/";
+  static final _options = BaseOptions(baseUrl: _baseUrl);
   static final _dio = Dio(_options);
 
   Future<RestaurantListResponse> getRestaurantList() async {
     final response = await _dio.get("/list");
     return RestaurantListResponse.fromJson(response.data);
   }
+
+  String getLowResPictureUrl(String pictureId) =>
+      "$_baseUrl/images/small/$pictureId";
+
+  String getMediumResPictureUrl(String pictureId) =>
+      "$_baseUrl/images/medium/$pictureId";
+
+  String getHighResPictureUrl(String pictureId) =>
+      "$_baseUrl/images/large/$pictureId";
 }
