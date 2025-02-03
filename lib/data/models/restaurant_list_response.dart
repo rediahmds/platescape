b2776e4b5c83f45246310928ad1b59e505c7fc7b
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-03 15:23:49 +0700

    feat(models): :sparkles: restaurant and list of restaurants

---
+++ lib/data/models/restaurant_list_response.dart
@@ -0,0 +1,15 @@
+import 'package:platescape/data/models/models.dart';
+
+class RestaurantListResponse {
+  bool error;
+  String message;
+  int count;
+  List<Restaurant> restaurants;
+
+  RestaurantListResponse({
+    required this.error,
+    required this.message,
+    required this.count,
+    required this.restaurants,
+  });
+}
