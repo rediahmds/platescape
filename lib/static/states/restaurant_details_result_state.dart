9374f88763ce04b6b82f0e5886a552ad78545003
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-04 17:32:00 +0700

    feat(states): :sparkles: restaurant details result

---
+++ lib/static/states/restaurant_details_result_state.dart
@@ -0,0 +1,19 @@
+import 'package:platescape/data/models/models.dart';
+
+sealed class RestaurantDetailsResultState {}
+
+class RestaurantDetailsNoneState extends RestaurantDetailsResultState {}
+
+class RestaurantDetailsLoadingState extends RestaurantDetailsResultState {}
+
+class RestaurantDetailsErrorState extends RestaurantDetailsResultState {
+  final String error;
+
+  RestaurantDetailsErrorState(this.error);
+}
+
+class RestaurantDetailsLoadedState extends RestaurantDetailsResultState {
+  final List<Restaurant> data;
+
+  RestaurantDetailsLoadedState(this.data);
+}
