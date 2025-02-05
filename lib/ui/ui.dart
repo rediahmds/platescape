c6f6f2e2332c03f264aa7ca2a6705b99092cd00c
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-06 02:24:49 +0700

    chore(barrel): :hammer: assign new ui components

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -1,1 +1,5 @@
+export 'restaurant_card/restaurant_card.dart';
 export 'restaurant_list_view.dart';
+export 'restaurant_location_detail/restaurant_location_detail.dart';
+export 'restaurant_ratings/restaurant_ratings.dart';
+export 'restaurant_tile/restaurant_tile.dart';
