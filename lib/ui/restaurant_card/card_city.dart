b8b4635214e8be29dbeb7f25cd28537762ab0b45
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-06 02:16:16 +0700

    feat(ui): :sparkles: update icon as per the content
    the content describe a city an thus uses city icon as a new one

--- lib/ui/restaurant_card/card_city.dart
+++ lib/ui/restaurant_card/card_city.dart
@@ -9,7 +9,7 @@ class CardCity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(Icons.location_pin),
+        Icon(Icons.location_city_rounded),
         const SizedBox.square(dimension: 4.0),
         Text(
           city,
