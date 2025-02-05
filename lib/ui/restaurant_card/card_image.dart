1a39338dd3efd11c5d1086c720903df4588bd9c6
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-06 02:17:49 +0700

    refactor(widget): :recycle: improve reusability
    added several optional props to increase reusabilty

--- lib/ui/restaurant_card/card_image.dart
+++ lib/ui/restaurant_card/card_image.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CardImage extends StatelessWidget {
-  const CardImage({super.key, required this.pictureUrl});
+  const CardImage(
+      {super.key,
+      required this.pictureUrl,
+      this.minWidth = 80,
+      this.minHeight = 80,
+      this.maxHeight = 120,
+      this.maxWidth = 120,
+      this.borderRadius = 15.0});
 
   final String pictureUrl;
+  final double minWidth;
+  final double minHeight;
+  final double maxWidth;
+  final double maxHeight;
+  final double borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints: const BoxConstraints(
-          minWidth: 80, minHeight: 80, maxWidth: 120, maxHeight: 120),
+      constraints: BoxConstraints(
+          minWidth: minWidth,
+          minHeight: minHeight,
+          maxWidth: maxWidth,
+          maxHeight: maxHeight),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(15.0),
+        borderRadius: BorderRadius.circular(borderRadius),
         child: Image.network(
           pictureUrl,
           fit: BoxFit.cover,
