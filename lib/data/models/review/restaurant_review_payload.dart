6d863fe510304e327a5bdb64545e90da7cd4a39d
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-08 15:00:48 +0700

    chore(models): :hammer: restaurant review models

---
+++ lib/data/models/review/restaurant_review_payload.dart
@@ -0,0 +1,11 @@
+class RestaurantReviewsPayload {
+  final String id;
+  final String name;
+  final String review;
+
+  RestaurantReviewsPayload({
+    required this.id,
+    required this.name,
+    required this.review,
+  });
+}
