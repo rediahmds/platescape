6e69645addd9cd084b9ab520dd764d849c99f6d4
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-22 17:28:44 +0700

    feat(ui): :sparkles: change theme via alert dialog

--- lib/ui/theme_settings/theme_settings.dart
+++ lib/ui/theme_settings/theme_settings.dart
@@ -1,1 +1,3 @@
+export 'theme_alert_dialog.dart';
+export 'theme_option_radio_tile.dart';
 export 'theme_tile.dart';
--- lib/ui/theme_settings/theme_tile.dart
+++ lib/ui/theme_settings/theme_tile.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:platescape/providers/providers.dart';
+import 'package:platescape/ui/ui.dart';
+import 'package:provider/provider.dart';
 
 class ThemeTile extends StatelessWidget {
   const ThemeTile({super.key});
 
+  static const String _tileTitle = "Theme";
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("Theme"),
-      subtitle: const Text("System default"),
+      title: Text(_tileTitle),
+      subtitle: Text(
+        context.watch<ThemeProvider>().themeMode.name,
+      ),
       onTap: () {
-        debugPrint("Setting theme...");
+        _showThemeAlertDialog(context, _tileTitle);
       },
+    );
+  }
+
+  void _showThemeAlertDialog(BuildContext context, String dialogTitle) {
+    showDialog(
+      context: context,
+      builder: (context) => ThemeAlertDialog(dialogTitle: dialogTitle),
     );
   }
 }
