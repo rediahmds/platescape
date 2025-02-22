8653edc42afa2360287ea04291857449bfc92c75
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-22 17:28:02 +0700

    fix(prefs): :bug: flutter binding not initialized

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
 
   runApp(MultiProvider(
