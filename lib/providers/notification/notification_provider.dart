f87105afafe2cb2df47be64e2538d076726a6cf6
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-23 21:50:44 +0700

    feat(providers): :sparkles: list and cancel upcoming notifications

--- lib/providers/notification/notification_provider.dart
+++ lib/providers/notification/notification_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:platescape/data/data.dart';
 
 class NotificationProvider extends ChangeNotifier {
@@ -10,10 +11,25 @@ class NotificationProvider extends ChangeNotifier {
   bool _permission = false;
   bool get permission => _permission;
 
+  List<PendingNotificationRequest> _pendingNotifications = [];
+  List<PendingNotificationRequest> get pendingNotifications =>
+      _pendingNotifications;
+
   Future<void> scheduleDailyNotification() async {
     _notificationId++;
     await _notificationService.scheduleDailyNotification(
       id: _notificationId,
     );
+  }
+
+  Future<void> getPendingNotifications() async {
+    _pendingNotifications =
+        await _notificationService.getPendingNotifications();
+
+    notifyListeners();
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await _notificationService.cancelNotification(id);
   }
 }
