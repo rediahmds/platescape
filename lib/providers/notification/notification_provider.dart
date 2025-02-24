625785446d0c7dabb183d1f8b208302fffddf198
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-24 12:37:32 +0700

    feat(providers): :sparkles: toggle notification on or off
    save the value to preferences

--- lib/providers/notification/notification_provider.dart
+++ lib/providers/notification/notification_provider.dart
@@ -3,19 +3,49 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:platescape/data/data.dart';
 
 class NotificationProvider extends ChangeNotifier {
-  NotificationProvider(this._notificationService);
+  NotificationProvider(this._notificationService, this._preferencesService) {
+    _loadNotificationStatus();
+  }
+
+  // TODO: Refactor to implement try catch block
+
   final NotificationService _notificationService;
+  final PreferencesService _preferencesService;
 
   int _notificationId = 0;
 
   bool _permission = false;
   bool get permission => _permission;
 
+  bool _isNotificationEnabled = false;
+  bool get isNotificationEnabled => _isNotificationEnabled;
+
+  String _message = "";
+  String get message => _message;
+
   List<PendingNotificationRequest> _pendingNotifications = [];
   List<PendingNotificationRequest> get pendingNotifications =>
       _pendingNotifications;
 
-  Future<void> scheduleDailyNotification() async {
+  void _loadNotificationStatus() {
+    _isNotificationEnabled = _preferencesService.getNotification();
+    notifyListeners();
+  }
+
+  Future<void> toggleNotification(bool value) async {
+    _isNotificationEnabled = value;
+    await _preferencesService.saveNotification(value);
+
+    if (_isNotificationEnabled) {
+      await _scheduleDailyNotification();
+    } else {
+      await cancelAllNotifications();
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> _scheduleDailyNotification() async {
     _notificationId++;
     await _notificationService.scheduleDailyNotification(
       id: _notificationId,
